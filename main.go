2e230d8f687b4a433fc2f8951c6b45b67d2bc37a
mesh-dell <mwaimonicas@gmail.com>
2025-11-13 14:24:49 +0300

    initialize main application with HTTP server and route handlers

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/mesh-dell/personal-blog/internal/handlers"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/add", handlers.AuthMiddleware(handlers.AddArticleHandler))
+	http.HandleFunc("/admin", handlers.AuthMiddleware(handlers.AdminHandler))
+	http.HandleFunc("/delete/{id}", handlers.AuthMiddleware(handlers.DeleteArticleHandler))
+	http.HandleFunc("/edit/{id}", handlers.AuthMiddleware(handlers.UpdateArticleHandler))
+	http.HandleFunc("/article/{id}", handlers.ArticleViewHandler)
+
+	fmt.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
